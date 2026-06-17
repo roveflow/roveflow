db587bf8ac7bf1a12eb8c6b557b19c228ca169d3
roveflow-release <release@roveflow>
2026-06-17 19:17:36 +0400

    Release v0.2.1

--- examples/counter_app/android/app/src/main/kotlin/com/roveflow/counter_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.roveflow.counter_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- examples/nav_app/android/app/src/main/kotlin/com/roveflow/nav_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.roveflow.nav_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
